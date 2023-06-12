78d3303d69a4f9a08d02071b9ac302e32fd53a76
Renzo Arredondo <renzo.mau1988@gmail.com>
2023-06-12 20:52:37 -0300

    clase 3 en Phyton

---
+++ Phyton2023/Leccion1/main.py
@@ -0,0 +1,33 @@
+'''miVariable = 3
+print(miVariable)
+miVariable = "a"
+print(miVariable)
+x = 10
+y = 20
+z = x+y
+print(z)
+
+#resultado= input("Digite un numero: ")
+#print(resultado)
+
+numero1 = int(input("Digite un numero: "))
+numero2 = int(input("Digite un numero 2: "))
+
+resultado = numero1+numero2
+print("El resultado de la suma es: ",resultado)'''
+
+operandoA=8
+operandoB=5
+suma=operandoA+operandoB
+
+print ("El resultado de la suma es ",suma)
+print(f"El resultado de la suma es: {suma}")
+
+resta=operandoA-operandoB
+print(f"El resultado de la suma es: {resta}")
+
+multiplicacion=operandoA*operandoB
+print(f"El resultado de la suma es: {multiplicacion}")
+
+division=operandoA/operandoB
+print(f"El resultado de la suma es: {division}")
